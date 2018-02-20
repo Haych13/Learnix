9bb706700d611a4946f7ddc6a0c0e10aa7059787
Haych <a7whihar@gmail.com>
2018-02-20 13:05:33 +0000

    Changes size of Learnix and made it unable to be resized
    Unconsistant sizes throughout the scenes, now set to 1000x700 pixels, added code to stop resizing so it stays consistant. Doesn't scale properly on resize so also why. Also left a line (although commented) that removes the OS window around the program, could be used for "fullscreen" applications or make my own window for a more professional look.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import javafx.scene.image.Image;
+import javafx.stage.StageStyle;
 
 public class Main extends Application
 {
@@ -21,11 +22,13 @@ public class Main extends Application
     public void start(Stage primaryStage) throws Exception
     {
         //window = primaryStage;
-        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml")); //This is the first FXML file that will open
         primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("icon.png"))); //loads image as the program's icon
         //primaryStage.getIcons().add(new Image("https://harrywhitter.ovh/wp-content/uploads/sharex/icon.png")); //Uploaded file to my own website, can load the Learnix icon from there instead if wanted
         primaryStage.setTitle("Learnix"); // Names the program Learnix
-        primaryStage.setScene(new Scene(root, 800, 500));  //default size
+        primaryStage.setScene(new Scene(root, 1000, 700));  //default size for the program to load into
+        //primaryStage.initStyle(StageStyle.UNDECORATED); //Removes the Windows bar completely
+        primaryStage.setResizable(false); // Stops the resizing and maximising of the program
         primaryStage.show();
 
     }
