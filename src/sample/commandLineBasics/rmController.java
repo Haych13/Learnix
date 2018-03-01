f34a5921e619d60b1c179872087e2721e6701543
Haych13 <a7whihar@gmail.com>
2018-03-01 23:08:36 +0000

    Added Next button function to latest tutorial 'rm'

--- src/sample/commandLineBasics/rmController.java
+++ src/sample/commandLineBasics/rmController.java
@@ -9,13 +9,21 @@ import java.io.IOException;
 public class rmController
 {
     public Button backBtn;
-
-    public void backBtnClick() throws IOException {
+    public void backBtnClick() throws IOException
+    {
         System.out.println("You are back to the lessons screen");
         Stage primaryStage = (Stage) backBtn.getScene().getWindow();
         Parent newRoot = FXMLLoader.load(getClass().getResource("lessons.fxml"));
         primaryStage.getScene().setRoot(newRoot);
     }
 
+    public Button nextBtn;
+    public void backBtnClick() throws IOException
+    {
+      System.out.println("You clicked the Next button");
+      Stage primaryStage = (Stage) nextBtn.getScene().getWindow();
+      Parent newRoot = FXMLLoader.load(getClass().getResource("lessons.fxml"));
+      primaryStage.getScene().setRoot(newRoot);
+    }
 
 }
