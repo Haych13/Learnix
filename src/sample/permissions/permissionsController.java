49bf4e4146f68964cda85b15534c8a5b8184ed1d
Haych <a7whihar@gmail.com>
2018-03-07 13:52:19 +0000

    Modified files

--- src/sample/permissions/permissionsController.java
+++ src/sample/permissions/permissionsController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package sample;
 
 import javafx.fxml.FXMLLoader;
@@ -20,26 +19,3 @@ public class permissionsController
 
 
 }
-=======
-package sample;
-
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.control.Button;
-import javafx.stage.Stage;
-import java.io.IOException;
-
-public class permissionsController
-{
-    public Button backBtn;
-
-    public void backBtnClick() throws IOException {
-        System.out.println("You are back to the lessons screen");
-        Stage primaryStage = (Stage) backBtn.getScene().getWindow();
-        Parent newRoot = FXMLLoader.load(getClass().getResource("lessons.fxml"));
-        primaryStage.getScene().setRoot(newRoot);
-    }
-
-
-}
->>>>>>> 4821a1a2c7652c0e08cb732d0b7076c437f54ff4
