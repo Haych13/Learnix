55b13cfc19fefa079da8abeca23e4afeeb1f8360
Harry Whitter <a7whihar@gmail.com>
2018-03-20 16:08:02 +0000

    Added new files ready for editing to be real tutorials. Copied from Users and Groups as a template

---
+++ src/sample/usersAndGroups/managingUsersController.java
@@ -0,0 +1,21 @@
+package sample;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class managingUsersController
+{
+    public Button backBtn;
+
+    public void backBtnClick() throws IOException {
+        System.out.println("You are back to the lessons screen");
+        Stage primaryStage = (Stage) backBtn.getScene().getWindow();
+        Parent newRoot = FXMLLoader.load(getClass().getResource("lessons.fxml"));
+        primaryStage.getScene().setRoot(newRoot);
+    }
+
+
+}
